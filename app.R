034ebd7b31e99e55f8567de92de90bbdb66ede50
mwesner2015 <49595041+mwesner2015@users.noreply.github.com>
2019-04-15 12:41:09 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,24 +1,63 @@
-library(readr)
-library(ggplot2)
+if("DT" %in% rownames(installed.packages()) == FALSE) {install.packages("DT")}
+if("tidyr" %in% rownames(installed.packages()) == FALSE) {install.packages("tidyr")}
+if("ggplot2" %in% rownames(installed.packages()) == FALSE) {install.packages("ggplot2")}
+if("dplyr" %in% rownames(installed.packages()) == FALSE) {install.packages("dplyr")}
+if("shiny" %in% rownames(installed.packages()) == FALSE) {install.packages("shiny")}
+if("readr" %in% rownames(installed.packages()) == FALSE) {install.packages("readr")}
+if("reshape" %in% rownames(installed.packages()) == FALSE) {install.packages("reshape")}
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(DT)
+library(shiny)
+library(readr)
+library(reshape)
+
 breweries <- read_csv("data/breweries.csv")
 beers <- read_csv("data/beers.csv")
 
-Beer = select(Beer,-X1)
+beers = select(beers,-X1)
+beers[is.na(beers)] <- 0
 
-breweries <- read_csv("breweries.csv")
-beers <- read_csv("beers.csv")
 
-Beer = select(Beer,-X1)
+beers[is.na(beers)] <- 0
 
-Cleanbrewery = Brewery %>%
+clean_breweries = breweries %>%
   rename(brewery = name) %>%
   rename(brewery_id = X1) %>%
   mutate(brewery_id = brewery_id + 1)
 
-Cleanbeer = Beer %>%
+clean_beer = beers %>%
   rename(beer = name)
 
-Beers = full_join(Cleanbrewery,Cleanbeer, by = 'brewery_id')
-Beers
+beer = full_join(clean_breweries, clean_beer, by = 'brewery_id')
+
+ui = fluidPage(
+  titlePanel("Craft Beer in the US"),
+  
+  # Create a new Row in the UI for selectInputs
+  fluidRow(
+    column(4,
+           selectInput("brewery",
+                       "Choose a Brewery:",
+                       c("All",
+                         unique(as.character(beer$brewery))))
+    )
+  ),
+  # Create a new row for the table.
+  DT::dataTableOutput("table")
+)
+
+server = function(input, output) {
+  
+  # Filter data based on selections
+  output$table <- DT::renderDataTable(DT::datatable({
+    data = beer
+    if (input$brewery != "All") {
+      data <- data[data$brewery == input$brewery,]
+    }
+    data
+  }))
+  
+}
+shinyApp(ui = ui, server = server)

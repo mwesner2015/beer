bf1ef51b34c580a3a81aba312a2499c7a7d7fb96
mwesner2015 <49595041+mwesner2015@users.noreply.github.com>
2019-04-15 21:09:54 -0400

    Update app.R

--- app.R
+++ app.R
@@ -30,6 +30,7 @@ clean_beer = beers %>%
 beer = full_join(clean_breweries, clean_beer, by = 'brewery_id')
 beer[[5]] = NULL
 beer[[1]] = NULL
+beer[["id"]] = NULL
 beer = beer[!is.na(beer$brewery), ]
 
 ui = fluidPage(
